235f117794f1c8ba6e97a8214489a47e35550446
Laur <laurentiuvieru50@gmail.com>
2023-02-08 21:34:48 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./assets/logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lorem 15 asd asdasd</h1>
+      <img src={logo} alt="Logo" width={80} style={{ borderRadius: "25px" }} />
+    </div>
+  );
+}
+
+export default App;
